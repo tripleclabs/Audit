11023912549f2bc4dd44d37352428b73895f43c0
John Maxwell <jm@wearecococo.com>
2026-02-27 13:13:06 +0100

    remove env dependency

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,16 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+let _db: PostgresJsDatabase<typeof schema> | null = null;
 
-const client = postgres(env.DATABASE_URL);
-
-export const db = drizzle(client, { schema });
+export const db = new Proxy({} as PostgresJsDatabase<typeof schema>, {
+	get(_, prop) {
+		if (!_db) {
+			if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+			_db = drizzle(postgres(env.DATABASE_URL), { schema });
+		}
+		return Reflect.get(_db, prop);
+	}
+});
