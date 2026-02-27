163ccf5c6f8bcc6933ee877b1cdcfe1225cf9b6a
John Maxwell <jm@wearecococo.com>
2026-02-27 13:19:30 +0100

    fix: db getter

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,12 +5,16 @@ import { env } from '$env/dynamic/private';
 
 let _db: PostgresJsDatabase<typeof schema> | null = null;
 
+function getDb() {
+	if (!_db) {
+		if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+		_db = drizzle(postgres(env.DATABASE_URL), { schema });
+	}
+	return _db;
+}
+
 export const db = new Proxy({} as PostgresJsDatabase<typeof schema>, {
 	get(_, prop) {
-		if (!_db) {
-			if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-			_db = drizzle(postgres(env.DATABASE_URL), { schema });
-		}
-		return Reflect.get(_db, prop);
+		return Reflect.get(getDb(), prop);
 	}
 });
